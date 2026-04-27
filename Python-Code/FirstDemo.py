e71a9a9824b7dd72bd21174ace72a52e4057bb64
Chandra <chandra27.amuda@gmail.com>
2026-04-26 19:23:23 -0500

    My First Commit to Python-Playwright Code Base.

---
+++ Python-Code/FirstDemo.py
@@ -0,0 +1,13 @@
+print("Hello, this is first Python Program.")
+
+# we can start with hash and then write any comments in the python file
+
+# Simple way of creating variable in Python
+a = 3
+print(a)
+
+str = "Hello World"
+print(str)
+
+#we can actually define multiple variables in a single line
+b,c, d = 5, 6.4, "Great"
