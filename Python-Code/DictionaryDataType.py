f43e27b67ba49f64cbb7321e218a98ed87413e7f
Chandrakanth Amuda <chandra27.amuda@gmail.com>
2026-05-03 15:33:21 -0500

    Python Basics, and Data Types.

---
+++ Python-Code/DictionaryDataType.py
@@ -0,0 +1,9 @@
+#Declare a dictionary value with key:value assign to it
+
+dictValues = {"a":2, 4:"bcd", "c":"Hello World"}
+
+#To print specific Key value from the dictionary 
+
+print(dictValues[4]) # o/p: bcd
+
+print(dictValues['c']) #o/p: Hello World
---
+++ Python-Code/TupleDataType.py
@@ -0,0 +1,8 @@
+values = (1, 2, "Python", 4.5)
+
+#To get the values for a specific index.
+print(values[2])
+
+#Now if we try to value of a Tuple at particular index, then it throw an error saying "TypeError: 'tuple' object does not support item assignment"
+
+values[2] = 'PYTHON'
